6f5a4db781434d307909c0f9801fd06b6436057a
Dan Koskiranta <G00397054@atu.ie>
2022-10-10 11:36:06 +0100

    add

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Calc
 {
+
     public static void main(String[] args) {
         add();
         delete();
+        multiply();
     }
 
+    // Add two numbers
     public static void add()
     {
         System.out.println("Please enter your first number: ");
@@ -22,16 +25,31 @@ public class Calc
         System.out.println("The total is " + total);
     }
 
+    // Subtract two numbers
     public static void delete()
     {
-        System.out.println("Please enter your first number: ");
+        System.out.println("Please enter your third number: ");
         Scanner inputs = new Scanner(System.in);
-        int firstNum = inputs.nextInt();
+        int firstNumber = inputs.nextInt();
 
-        System.out.println("Please enter the second number: ");
-        int secondNum = inputs.nextInt();
+        System.out.println("Please enter the fourth number: ");
+        int secondNumber = inputs.nextInt();
 
-        int subtraction = firstNum - secondNum;
-        System.out.println("The result is " + subtraction);
+        int delete = firstNumber - secondNumber;
+        System.out.println("The subtraction is " + delete);
+    }
+
+
+    // Multiply two numbers
+    public static void multiply()
+    {
+        System.out.println("Please enter your fifth number: ");
+        Scanner inputs = new Scanner(System.in);
+        int oneNum = inputs.nextInt();
+
+        System.out.println("Please enter the sixth number: ");
+        int twoNum = inputs.nextInt();
+        int multiple = oneNum * twoNum;
+        System.out.println("The multiple is " + multiple);
     }
 }
